d765afc4d9353f321cb905ab0c6d24d8be10697f
JD-2104 <jdanso019@st.ug.edu.gh>
2025-08-09 01:46:16 +0000

    Task 1 committed

---
+++ DCIT318_Assignment2/DCIT318_Assignment2/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Animal genericAnimal = new Animal();
+        genericAnimal.MakeSound();
+
+        Dog dog = new Dog();
+        dog.MakeSound();
+
+        Cat cat = new Cat();
+        cat.MakeSound();
+    }
+}
