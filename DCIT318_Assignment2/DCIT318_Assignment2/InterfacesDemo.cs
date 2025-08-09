4c02f2c00fbae5116ffb0c9dc43df48d72c8202b
JD-2104 <jdanso019@st.ug.edu.gh>
2025-08-09 02:12:41 +0000

    Task 3 committed

---
+++ DCIT318_Assignment2/DCIT318_Assignment2/InterfacesDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public interface IMovable
+{
+    void Move();
+}
+
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+public class ProgramTask3
+{
+    public static void Main(string[] args)
+    {
+        IMovable myCar = new Car();
+        IMovable myBike = new Bicycle();
+
+        myCar.Move();
+        myBike.Move();
+    }
+}
